973c42703bdc0812a247568234f740ffb2a081a9
honeypotfly <honeypotfly@protonmail.com>
2022-03-20 15:38:43 -0400

    add some logging

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -1,5 +1,5 @@
 const {readFileSync, writeFileSync} = require('fs')
-
+console.log('start')
 const first = readFileSync('./content/first.txt', 'utf8')
 const second = readFileSync('./content/second.txt', 'utf8')
 
@@ -7,3 +7,5 @@ writeFileSync('./content/result-sync.txt',
  `Here is the result ${first}, ${second}`,
  {flag: 'a'}
  )
+ console.log('done with this task')
+ console.log('starting with the next task')
