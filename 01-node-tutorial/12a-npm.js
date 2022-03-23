49e8f01288694c1635f7bd42d1fef2764404818a
honeypotfly <honeypotfly@protonmail.com>
2022-03-22 22:48:34 -0400

    move files over

---
+++ 01-node-tutorial/12a-npm.js
@@ -0,0 +1,21 @@
+// npm - global command, which comes with node
+// npm --version
+
+// local dependency - for local project
+// npm i <package>
+
+// global dep - use it in any project on your host
+// npm install -g <package>
+
+// package.json - manifest file (stores info about project/package)
+// manual approach, create package.json in doc root
+// npm init (step by step)
+// npm init -y (all defaults)
+
+const _ = require("lodash");
+const items = [1, [2, [3, [4, 5]]]];
+
+const newItems = _.flattenDeep(items);
+console.log(newItems);
+
+console.log("Hello worldy");
